492d7006abfbe0c6eea1797b82f819506b9e3663
tuangt <Tuang@Tuangs-Mac.local>
2019-01-27 12:58:23 -0800

    create canvas

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
   fill(255,0,0);
   rect(30,30,100,100);
+  fill("BLACK");
+  text("This is Bravo App. and we will connect it to Sqaure.",30,200);
+  text("Also, testing fullscreen right now. No scrollbar",30,220);
+}
+
+function button(){
+
 }
